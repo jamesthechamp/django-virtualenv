c3582bc2458476d316f578bdad8b7af5480cbfa8
james <bajaj.shivansh@rocketmail.com>
2016-01-08 23:30:44 +0530

    changes made

--- lnmiit_chat/chat/urls.py
+++ lnmiit_chat/chat/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
- 
-from chat import views
- 
-urlpatterns = patterns('', 
-	url(r'^$', views.index, name='index'),
-        url(r'^chat_room/', views.chat_room, name='chat_room'),
-       
-)
 
+from chat import views
+
+urlpatterns = patterns('',
+	url(r'^$', views.index, name='index'),
+        url(r'^chat_room/(\d+)/$', views.chat_room, name='chat_room'),
+
+)
